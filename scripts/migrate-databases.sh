dae44c5eba1b67222ec6c5f4ce4cca4984118e74
Suraj Sriwastava <suraj_sriwastava@dell.com>
2019-01-08 18:33:21 +0530

    updated service name

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -3,7 +3,7 @@ set -e
 
 
 app_guid=`cf app $1 --guid`
-credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES | if .["p-mysql"] != null then .["p-mysql"] else .["p.mysql"] end | .[0].credentials'`
+credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES | if .["cleardb"] != null then .["cleardb"] else .["cleardb"] end | .[0].credentials'`
 
 ip_address=`echo $credentials | jq -r '.hostname'`
 db_name=`echo $credentials | jq -r '.name'`
